ea5fdaa45025629aa2081450e61e5f264df7420a
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-24 17:47:31 -0300

    Add Home and NavBar compoment

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+//Hot key: sfc
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dojo Blob</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+//Hot key: sfc
+
+export default NavBar;
