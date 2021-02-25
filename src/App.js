40254d5064f84e0f001dc36d6c3ebbad54e7d396
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-25 09:16:56 -0300

    adding custom style in style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import NavBar from './Navbar'
 import Home from './Home'
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,11 @@ const NavBar = () => {
             <h1>The Dojo Blob</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:'#f1356d',
+                    borderRadius:'8px'
+                }}>New Blog</a>
             </div>
         </nav>
      );
