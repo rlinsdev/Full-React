defba1a13f52285bcb6021171d7ef0867507dcc3
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-26 17:32:13 -0300

    buttons with param and no param

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>HomePage</h2>
-        </div>
-     );
-}
- 
+  
+  // 
+  const handleClick = () => {
+    console.log('teset');
+  };
+
+  const handleClickParameter = (param) =>{
+    console.log(`Hello ${param}`)
+  }
+
+  return (
+    <div className='home'>
+      <h2>HomePage</h2>
+      <button onClick={handleClick}>Clicke me</button>
+      <button onClick={()=>{handleClickParameter('teste!')}}>button 2</button>
+    </div>
+  );
+};
+
 export default Home;
