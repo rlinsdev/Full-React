9192362297b1172dc74a7edd505a68ec21464697
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-27 10:14:33 -0300

    interaction with list

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('Mario');
-
-  
-  const handleClick = () => {
-    setName('Luigi')
-  };
-
+  const [blobs, setBlobs] = useState([
+    { title: 'My new WebSite - 001', body:'lorem ipsum...', author:'Mario', id:1 },
+    { title: 'My new WebSite - 002', body:'lorem ipsum...', author:'Luiggi', id:2 },
+    { title: 'My new WebSite - 003', body:'lorem ipsum...', author:'Toddy', id:3 },
+  ])
   return (
     <div className='home'>
-      <h2>HomePage</h2>
-      <button onClick={handleClick}>Clicke me</button>
-      <p>{name}</p>
+    {blobs.map((blog) => (
+      <div className="blog-preview" key={blog.id}>
+        <h2> { blog.title } </h2>
+        <p>Writen by { blog.author }</p>
+      </div>
+    ))}
     </div>
   );
 };
