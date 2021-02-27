b5f7838ba42fdd91ee8ee0b6bf1bc7328c65fc6e
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-27 10:54:08 -0300

    using props in react

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,15 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  const [blobs, setBlobs] = useState([
+  const [blogs, setBlogs] = useState([
     { title: 'My new WebSite - 001', body:'lorem ipsum...', author:'Mario', id:1 },
     { title: 'My new WebSite - 002', body:'lorem ipsum...', author:'Luiggi', id:2 },
     { title: 'My new WebSite - 003', body:'lorem ipsum...', author:'Toddy', id:3 },
   ])
   return (
     <div className='home'>
-    {blobs.map((blog) => (
-      <div className="blog-preview" key={blog.id}>
-        <h2> { blog.title } </h2>
-        <p>Writen by { blog.author }</p>
-      </div>
-    ))}
+    <BlogList blogs={blogs} />
     </div>
   );
 };
