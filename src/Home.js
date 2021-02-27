7277443357fe0f7c7bf437d673792882625133f5
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-27 14:58:12 -0300

    removing a item from list

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new WebSite - 001', body:'lorem ipsum...', author:'Mario', id:1 },
-    { title: 'My new WebSite - 002', body:'lorem ipsum...', author:'Luiggi', id:2 },
-    { title: 'My new WebSite - 003', body:'lorem ipsum...', author:'Toddy', id:3 },
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return (
-    <div className='home'>
-    <BlogList blogs={blogs} />
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
   );
-};
-
+}
+ 
 export default Home;
