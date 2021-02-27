fafc3fd51fa8dd139646f339e71d429373ba716f
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2021-02-27 15:34:54 -0300

    Use Effect with dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,14 +8,24 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
+  const [name, setName] = useState('Mario')
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
 
+  useEffect(()=>{
+    console.log('use effect ran');
+    console.log(name);
+
+  }, [name]);
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={()=>setName('luiggi')} >Change Name</button>
+      <p>{name}</p>
     </div>
   );
 }
