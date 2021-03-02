5a521a87c6b838ce9c487c0a2e9ab9cb592e6f1f
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-02 10:09:49 -0300

    linking inputs with react controllers

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState();
+  const [body, setBody] = useState();
+  const [author, setAuthor] = useState('Mario');
   return ( 
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input type="text" require='true' value={ title } onChange={ (e) => setTitle(e.target.value) }></input>
+        <label>Blog Body:</label>
+        <textarea require='true' value={ body } onChange={ (e)=> setBody(e.target.value) } ></textarea>
+        <label>Blog Author:</label>
+        <select value={author} onChange={ (e) => setAuthor(e.target.value) }>
+          <option value='Mario'>Mario</option>
+          <option value='Yoshy'>Yoshy</option>
+        </select>
+        <button>Add Blog</button>
+        {title} {body} {author}
+      </form>
     </div>
    );
 }
