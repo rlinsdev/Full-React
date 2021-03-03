46817a663e3194f611c417ecc4a75aea4be267cf
Rodrigo Lins <rlinsdev@gmail.com>
2021-03-02 22:12:48 -0300

    deleting registers

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
   const { id } = useParams();
-  const { data:blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`)
+  const { data:blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:8000/blogs/${id}`,{
+      method:'DELETE'
+    }).then(() => {
+      history.push('/')
+    });
+  }
 
   return ( 
     <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p><b>Written by: </b><i>{blog.author}</i></p>
           <div>{ blog.body }</div>
+          <button onClick={handleClick} >Delete</button>
         </article>
       )}</div>
     </div>
